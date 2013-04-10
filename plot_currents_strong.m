6b4193aca1ecef334d87c3e47fe1a0775fd567fc
Theo Thompson <theodore.thompson@students.olin.edu>
2013-04-10 00:37:57 -0400

    fuck you im sober

--- plot_currents_strong.m
+++ plot_currents_strong.m
@@ -8,6 +8,11 @@ plot(V_src,I1a,'ob'); hold on;
 plot(V_src,I1,'or'); 
 plot(V_src,I1a-I1,'ok');
 plot(V_src,I1a+I1,'og'); 
+I_diff=I1a-I1;
+inds=intersect(find(.05>V_src),find(V_src>-.05));
+p=polyfit(V_src(inds),I_diff(inds),1);
+'Transconductance Gain 2V'
+p(1)
 
 clear all
 load('V=3V_I1_meas_strong.mat');
@@ -18,6 +23,11 @@ plot(V_src,I1a,'.b'); hold on;
 plot(V_src,I1,'.r'); 
 plot(V_src,I1a-I1,'.k');
 plot(V_src,I1a+I1,'.g'); 
+I_diff=I1a-I1;
+inds=intersect(find(.05>V_src),find(V_src>-.05));
+p=polyfit(V_src(inds),I_diff(inds),1);
+'Transconductance Gain 3V'
+p(1)
 
 clear all
 load('V=4V_I1_meas_strong.mat');
@@ -28,9 +38,16 @@ plot(V_src,I1a,'*b'); hold on;
 plot(V_src,I1,'*r'); 
 plot(V_src,I1a-I1,'*k');
 plot(V_src,I1a+I1,'*g'); 
-legend('I1','I2','I1-I2','I1+I2','location','best')
 
-title('Currents as a function of V_{DM}','FontSize',14);
+I_diff=I1a-I1;
+inds=intersect(find(.05>V_src),find(V_src>-.05));
+p=polyfit(V_src(inds),I_diff(inds),1);
+'Transconductance Gain 4V'
+p(1)
+
+legend('I1 2V','I2 2V','I1-I2 2V','I1+I2 2V','I1 3V','I2 3V','I1-I2 3V','I1+I2 3V','I1 4V','I2 4V','I1-I2 4V','I1+I2 4V','location','best')
+
+title('I_1 and I_2 With Strongly Inverted Bias Transistor','FontSize',14);
 xlabel('V_{DM}','FontSize',14);
 ylabel('I','FontSize',14)
 
--- plot_currents_weak.m
+++ plot_currents_weak.m
@@ -1,30 +1,55 @@
 clear all
-load('V=2V_I1_meas_weak.mat');
-load('V=2V_I2_meas_weak.mat');
+load('V=2V_measI1_weak.mat');
+load('V=2V_measI2_weak.mat');
 figure(1);
 plot(V_diff,I1,'ob'); hold on;
 plot(V_diff,I2,'or'); 
 plot(V_diff,I1-I2,'ok');
 plot(V_diff,I1+I2,'og'); 
-legend('I1','I2','I1-I2','I1+I2','location','best')
+
+I_diff=I1-I2;
+inds=intersect(find(.05>V_diff),find(V_diff>-.05));
+p=polyfit(V_diff(inds),I_diff(inds),1);
+'Transconductance Gain 2V'
+p(1)
 
 clear all
-load('V=3V_I1_meas_weak.mat');
-load('V=3V_I2_meas_weak.mat');
+load('V=3V_measI1_weak.mat');
+load('V=3V_measI2_weak.mat');
 figure(1);
 plot(V_diff,I1,'.b'); hold on;
 plot(V_diff,I2,'.r'); 
 plot(V_diff,I1-I2,'.k');
 plot(V_diff,I1+I2,'.g'); 
-legend('I1','I2','I1-I2','I1+I2','location','best')
 
-% clear all
-% load('V=4V_I1_meas_weak.mat');
-% I1=I2;
-% load('V=4V_I2_meas_weak.mat');
-% figure(1);
-% plot(V_src,I1,'*b'); hold on;
-% plot(V_src,I2,'*r'); 
-% plot(V_src,I1-I2,'*k');
-% plot(V_src,I1+I2,'*g'); 
-% legend('I1','I2','I1-I2','I1+I2','location','best')
+I_diff=I1-I2;
+inds=intersect(find(.05>V_diff),find(V_diff>-.05));
+p=polyfit(V_diff(inds),I_diff(inds),1);
+'Transconductance Gain 3V'
+p(1)
+
+clear all
+load('V=4V_measI1_weak.mat');
+load('V=4V_measI2_weak.mat');
+figure(1);
+plot(V_diff,I1,'*b'); hold on;
+plot(V_diff,I2,'*r'); 
+plot(V_diff,I1-I2,'*k');
+plot(V_diff,I1+I2,'*g'); 
+
+I_diff=I1-I2;
+inds=intersect(find(.05>V_diff),find(V_diff>-.05));
+p=polyfit(V_diff(inds),I_diff(inds),1);
+'Transconductance Gain 3V'
+p(1)
+
+axis([-.3 .3 -2.5e-6 2.5e-6])
+
+legend('I1 2V','I2 2V','I1-I2 2V','I1+I2 2V','I1 3V','I2 3V','I1-I2 3V','I1+I2 3V','I1 4V','I2 4V','I1-I2 4V','I1+I2 4V','location','best')
+
+title('I_1 and I_2 With Weakly Inverted Bias Transistor','FontSize',14);
+xlabel('V_{DM}','FontSize',14);
+ylabel('I','FontSize',14)
+
+print '-depsc' currents_strong
+saveas(gcf,'currents_weak.png')
