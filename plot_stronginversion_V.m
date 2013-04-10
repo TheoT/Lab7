71e736e64227d74302046bdd7b6f5a04b909458d
dankearney <daniel.kearney@students.olin.edu>
2013-04-09 21:09:47 -0400

    added data and started voltage stuff

---
+++ plot_stronginversion_V.m
@@ -0,0 +1,25 @@
+clear all; clc; clf; hold on;
+
+load V=2V_V_meas_strong
+V_2V        = V;
+V_diff_1    = V_src;
+
+load V=3V_V_meas_strong
+V_3V        = V;
+V_diff_2    = V_src;
+
+load V=4V_V_meas_strong
+V_4V        = V;
+V_diff_3    = V_src;
+
+plot(V_diff_1, V_2V,'o');
+plot(V_diff_2, V_3V,'o');
+plot(V_diff_3, V_4V,'o');
+
+axis([-.35 .35 .7 3.2])
+xlabel 'V_{dm} (V)'
+ylabel 'V (V)'
+title('Common Node Voltage as a Function of V_{dm}', 'fontsize',20)
+legend('V_2=2V','V_2=3V','V_2=4V','location','northwest')
+
+print '-depsc' plot_strong_v
